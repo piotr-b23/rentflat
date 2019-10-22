0633dde43b8bbdb07e33a3851d76e8c2b4ff294b
Piotr <piotrekkk23@op.pl>
2019-10-22 10:43:56 +0200

    register completed

--- app/src/main/java/com/example/rentflat/ui/register/Register.java
+++ app/src/main/java/com/example/rentflat/ui/register/Register.java
@@ -3,14 +3,102 @@ package com.example.rentflat.ui.register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.rentflat.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register extends AppCompatActivity {
+
+    private EditText name, username, email, password, confirmedPassword;
+    private Button registerButton;
+    private static String URL_REGIST = "http://192.168.1.12/regist.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        name = findViewById(R.id.name);
+        username = findViewById(R.id.nickname);
+        email = findViewById(R.id.userEmail);
+        password = findViewById(R.id.password);
+        confirmedPassword = findViewById(R.id.confirmedPassword);
+        registerButton = findViewById(R.id.confirmRegistrationButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Regist();
+
+
+            }
+        });
+    }
+
+    private void Regist(){
+        final String name = this.name.getText().toString().trim();
+        final String username = this.username.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String succes = jsonObject.getString("success");
+                            if (succes.equals("1")){
+                                Toast.makeText(Register.this,"Zarejestrowano",Toast.LENGTH_SHORT).show();
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(Register.this,"Błąd" + e.toString(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Register.this,"Błąd" + error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("name",name);
+                params.put("username",username);
+                params.put("password",password);
+                params.put("email",email);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
     }
 }
